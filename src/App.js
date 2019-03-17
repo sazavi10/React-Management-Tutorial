17da9cbd895c89bea727065dab06e804199f6440
Ryan Kang <sazavi10@naver.com>
2019-03-18 01:16:16 +0900

    Create Customer Cumponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+{
+  'id': `1`,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '강후익',
+  'birthday' : '901222',
+  'gender' : '남자',
+  'job' : '대학원생'
+},
+{
+  'id': `2`,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길순',
+  'birthday' : '901222',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+{
+  'id': `3`,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이순신',
+  'birthday' : '901222',
+  'gender' : '남자',
+  'job' : '대학원생'
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat='logo'/>
-        <h2>Let`s Go df</h2>
+      <div>
+        {
+          customers.map (c => {return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.jpb}/>);})
+        }
       </div>
     );
   }
