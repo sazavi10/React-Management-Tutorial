2bf9d41fdace0cd55727ef59b75bbd76b8faf713
Ryan Kang <sazavi10@naver.com>
2019-03-19 03:09:20 +0900

    Create  Customer Database Table & Node.js Database Connection

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles =  theme => (
   {
@@ -22,34 +23,29 @@ const styles =  theme => (
   }
 )
 
-const customers = [
-{
-  'id': `1`,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '강후익',
-  'birthday' : '901222',
-  'gender' : '남자',
-  'job' : '대학원생'
-},
-{
-  'id': `2`,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '홍길순',
-  'birthday' : '901222',
-  'gender' : '남자',
-  'job' : '프로그래머'
-},
-{
-  'id': `3`,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '이순신',
-  'birthday' : '901222',
-  'gender' : '남자',
-  'job' : '대학원생'
-}
-]
-
 class App extends Component {
+  state = {
+      customers: "",
+      completed: 0
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi().then(res=> this.setState({customers : res}))
+    .catch( err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -66,7 +62,20 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map (c => {return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.jpb}/>);})}
+          {
+          this.state.customers ? this.state.customers.map (c => {return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})
+          : 
+          <TableRow>
+            <TableCell colSpan='6' align="center">
+              <CircularProgress
+              className={classes.progress}
+              variant="determinate"
+              value={this.state.completed}
+              color="secondary"
+         />
+         </TableCell>
+        </TableRow>
+        }
           </TableBody>
         </Table>
       </Paper>
